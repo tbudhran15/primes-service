39ad2889c506a7387439e6038a330df8d0bdf652
tbudhran15 <tarash0715@gmail.com>
2025-04-08 20:25:54 -0400

    Comment out artifact upload step

--- scripts/push-to-acr.sh
+++ scripts/push-to-acr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 az acr login --name practicums
-docker tag ghcr.io/YOUR_GITHUB_USERNAME/primes-service:$SERVICE_VERSION practicums.azurecr.io/primes-service:$SERVICE_VERSION
+docker tag ghcr.io/tbudhran15/primes-service:$SERVICE_VERSION practicums.azurecr.io/primes-service:$SERVICE_VERSION
 docker push practicums.azurecr.io/primes-service:$SERVICE_VERSION
 
